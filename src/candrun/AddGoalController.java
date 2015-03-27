afb828172c181769b4921cd61a7cddd7e20fe2c1
JB <JB@JBui-MacBook-Pro.local>
2015-03-27 23:16:58 +0900

    #5 close

---
+++ src/candrun/AddGoalController.java
@@ -0,0 +1,24 @@
+package candrun;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addGoal.cdr")
+public class AddGoalController extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+	
+		String goalContents = req.getParameter("goal_contents");
+		String taskContents = req.getParameter("task_contents");
+		
+		System.out.println("goal: "+goalContents+" task: "+ taskContents);
+		
+	}
+}
