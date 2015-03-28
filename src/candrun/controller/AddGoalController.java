47f415e4bcebf58493492e3ef7afa138358c9a55
yskoh <koh.yesl@gmail.com>
2015-03-28 13:06:51 +0900

    #6 closes makeGoad db connection

--- src/candrun/controller/AddGoalController.java
+++ src/candrun/controller/AddGoalController.java
@@ -1,12 +1,17 @@
-package candrun;
+package candrun.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import candrun.dao.GoalDAO;
+import candrun.model.Goal;
 
 @WebServlet("/addGoal.cdr")
 public class AddGoalController extends HttpServlet{
@@ -19,6 +24,19 @@ public class AddGoalController extends HttpServlet{
 		String taskContents = req.getParameter("task_contents");
 		
 		System.out.println("goal: "+goalContents+" task: "+ taskContents);
+		
+		Goal goal = new Goal(goalContents, "email");
+		
+		//여기서 dao를 가져와서 dao 에게 넘겨준다. 
+		
+		GoalDAO goalDAO = new GoalDAO();
+		Connection con = goalDAO.getConnection();
+		try {
+			goalDAO.addGoal(goal);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
