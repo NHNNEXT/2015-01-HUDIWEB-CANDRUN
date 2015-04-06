88e369042e49369296a094b52550c5acc48dfc3e
Ellen Seon <ellen24h@gmail.com>
2015-04-06 19:33:28 +0900

    #11 Finished SignUp Perfectly

--- src/candrun/controller/SignUpController.java
+++ src/candrun/controller/SignUpController.java
@@ -1,14 +1,13 @@
 package candrun.controller;
 
 import java.io.IOException;
-
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import candrun.dao.UserDAO;
 import candrun.user.User;
@@ -24,14 +23,21 @@ public class SignUpController extends HttpServlet{
 		String nickname = req.getParameter("nickname");
 		String password = req.getParameter("password");
 		
-		UserDAO userDAO = new UserDAO();
 		User user = new User(email, nickname, password);		
+		UserDAO userDao = new UserDAO();
+		try {
+			userDao.addUser(user);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	 
+
 
 		//System.out.println(email + " " + nickname + " " + password);
 		
 		//Connection con = userDAO.getConnection();
 		resp.sendRedirect("signIn.jsp");
 
-
+		
 	}
 }
