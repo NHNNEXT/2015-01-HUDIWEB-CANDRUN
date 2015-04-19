a71ec882cd33ed2628c0e88d763807819d32b940
yskoh <koh.yesl@gmail.com>
2015-04-19 23:06:58 +0900

    closes #39

--- src/candrun/dao/TaskDAO.java
+++ src/candrun/dao/TaskDAO.java
@@ -11,7 +11,17 @@ import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import candrun.model.Task;
 
 public class TaskDAO extends JdbcDaoSupport{
-
+	
+	private RowMapper<Task> rowMapper = new RowMapper<Task>() {
+		public Task mapRow(ResultSet rs, int rowNum) {
+			try {
+				return new Task(rs.getString("contents"), rs.getInt("goal_id"), rs.getInt("id"), rs.getInt("nudge"));
+			} catch (SQLException e) {
+				throw new BeanInstantiationException(Task.class, e.getMessage(), e);
+			}
+		}
+	};
+	
 	public void addTask(Task task) {
 		String sql ="INSERT INTO tasks(contents, goal_id) VALUES(?, ?)";
 		getJdbcTemplate().update(sql, task.getContents(), task.getGoalId());
@@ -20,17 +30,13 @@ public class TaskDAO extends JdbcDaoSupport{
 	//같은 goal아래 tasks을 불러온다
 	public List<Task> findTasksByGoalId(int goalId) {
 		String sql ="SELECT * FROM tasks WHERE goal_id = ? LIMIT 3";
-
-		RowMapper<Task> rowMapper = new RowMapper<Task>() {
-			public Task mapRow(ResultSet rs, int rowNum) {
-				try {
-					return new Task(rs.getString("contents"), rs.getInt("goal_id"), rs.getInt("id"), rs.getInt("nudge"));
-				} catch (SQLException e) {
-					throw new BeanInstantiationException(Task.class, e.getMessage(), e);
-				}
-			}
-		};
 		return getJdbcTemplate().query(sql, rowMapper, goalId);
+	}
+	
+	//TaskId로 해당 태스크를 가져온다. 
+	public Task findTaskByTaskId(int taskId) {
+		String sql = "SELECT * FROM tasks WHERE id = ?";
+		return getJdbcTemplate().queryForObject(sql, rowMapper, taskId);
 	}
 
 	//taskId를 갖고 와서 해당 task의 넛지 수를 하나 올려준다
