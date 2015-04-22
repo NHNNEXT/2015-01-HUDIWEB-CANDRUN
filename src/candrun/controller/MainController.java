13da2834ef31caaa6d830149f13ace7574ba133b
ellen24h <ellen24h@gmail.com>
2015-04-22 21:57:09 +0900

    #41 Modified MainController, UserDao

--- src/candrun/controller/MainController.java
+++ src/candrun/controller/MainController.java
@@ -2,6 +2,8 @@ package candrun.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import candrun.dao.GoalDAO;
 import candrun.dao.TaskDAO;
+import candrun.dao.UserDAO;
 import candrun.model.Goal;
 import candrun.model.Task;
+import candrun.user.User;
 
 @RequestMapping("/")
 @Controller
@@ -24,13 +28,23 @@ public class MainController {
 	@Autowired
 	TaskDAO taskDao;
 	
+	@Autowired
+	UserDAO userDao;
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list(Model model) {
-		
+	public String list(Model model, HttpSession session) {
+		String email = (String) session.getAttribute("email");
+
 		Goal topGoal = goalDao.findRecentGoal();
 		List<Task> tasks = taskDao.findTasksByGoalId(topGoal.getId());
+		List<User> friends = userDao.findFriendsAsRequester(email);
+		
+			
 		model.addAttribute("goal", topGoal);
 		model.addAttribute("tasks", tasks);
+		model.addAttribute("friends", friends);
+		
+		
 
 //		모델을 이용하여 attribute 더해준다. 아래는 이전 코드
 //		req.setAttribute("goal", topGoal);
--- src/candrun/dao/UserDAO.java
+++ src/candrun/dao/UserDAO.java
@@ -2,6 +2,7 @@ package candrun.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.BeanInstantiationException;
 import org.springframework.jdbc.core.RowMapper;
@@ -11,6 +12,20 @@ import candrun.user.User;
 
 public class UserDAO extends JdbcDaoSupport{
 		
+	private RowMapper<User> rowMapper = new RowMapper<User>() {
+		
+		public User mapRow(ResultSet rs, int rowNum) {
+			try {
+				return new User(
+						rs.getString("email"), 
+						rs.getString("nickname"),
+						rs.getString("password"));
+			} catch (SQLException e) {
+				throw new BeanInstantiationException(User.class, e.getMessage(), e);
+			}
+		}
+	};
+	
 	public void addPreliminaryUser(User user) {
 		String sql ="INSERT INTO preliminary_user(email, nickname, password, verify_key) VALUES(?, ?, ?, ?)";
 		getJdbcTemplate().update(sql,user.getEmail(), user.getNickname(), user.getPassword(), user.getVerifyKey());
@@ -23,22 +38,17 @@ public class UserDAO extends JdbcDaoSupport{
 
 	public User findByEmail(String email) {
 		String sql = "SELECT * FROM user WHERE email = ?";
-		RowMapper<User> rowMapper = new RowMapper<User>() {
-
-			public User mapRow(ResultSet rs, int rowNum) {
-				try {
-					return new User(
-							rs.getString("email"), 
-							rs.getString("nickname"),
-							rs.getString("password"));
-				} catch (SQLException e) {
-					throw new BeanInstantiationException(User.class, e.getMessage(), e);
-				}
-			}
-		};
 		return getJdbcTemplate().queryForObject(sql, rowMapper, email);
 	}
 
+//	user를 가져온다, status = 1이고, 내가 requester일 때, user_has_user에서 responser를 가져온다.
+//	그것으로 user table에서 email로 검색해서 user를 가져온다.
+	public List<User> findFriendsAsRequester(String email) {
+		String sql = "SELECT * from (SELECT * FROM user_has_user INNER JOIN user ON user_has_user.responser = user.email)temp WHERE requester = ? and status = 1"; 
+		
+		return getJdbcTemplate().query(sql, rowMapper, email);
+	}
+	
 	//TODO: nickname으로 verifyKey 생성시 중복 가능성이 있다.
 	public User findByVerifyKey(String verifyKey) {
 		String sql = "SELECT * FROM preliminary_user WHERE verify_key = ?";
@@ -58,4 +68,6 @@ public class UserDAO extends JdbcDaoSupport{
 		};
 		return getJdbcTemplate().queryForObject(sql, rowMapper, verifyKey);
 	}
+	
+	
 }
