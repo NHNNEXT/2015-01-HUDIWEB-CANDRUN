9fc4a96e6a384a62d0f9e207b50f35d479a4c1cc
cocagolau <cocagolau@gmail.com>
2015-05-12 11:46:08 +0900

    추가 등록

---
+++ src/candrun/support/enums/Value.java
@@ -0,0 +1,17 @@
+package candrun.support.enums;
+
+
+/*
+ * @see candrun.model.User
+ * @see candrun.model.UserTest
+ * @see candrun.service.UserService
+ * 
+ * @see candrun.support.enums.Value
+ * @see candrun.support.enums.UserErrorcode
+ * @see candrun.support.enums.CommonInvar
+ */
+public interface Value {
+	
+	String getValue();
+
+}
